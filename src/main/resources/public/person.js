3bd30616a81328bddfdc4777abd046caeb9ec881
Gianluca <giancroso@gmail.com>
2016-09-24 19:37:55 -0400

    person js

--- src/main/resources/public/person.js
+++ src/main/resources/public/person.js
@@ -1,27 +1,40 @@
-function person(userNames, Names, emails, condition, infoParagraphs) {
-	this.userName = userNames;
-	this.Name = Names;
+function Mentor(userNames, Gender, Max, Past, Names, Age, emails, condition1, condition2, condition3, psswrd, infoParagraphs) {
+	this.username = userNames;
+	this.fullName = Names;
+    this.age = Age
 	this.email = emails;
-	this.conditions = condition;
+	this.condition1 = condition1;
+	this.condition2 = condition2;
+	this.condition3 = condition3;
+	this.password = psswrd
+	this.gender = Gender;
+	this.max = Max;
+	this.past = Past;
 	this.infoParagraph = infoParagraphs;
-
 }
 
-
 var usrName = "msaltzm5";
+
+$.getJSON("supportpair/api/mentor/" + usrName, function (data) { alert(data) });
+
 var fullName = "Matthew Saltzman";
+var Gender = "M";
+var Max = 5;
+var Past = 1;
+var psswrd = "abc123";
+var Age = 22;
 var emailInput = "mattsaltzman01@gmail.com";
 var conditionsList = ["Leukemia", "Cancer", "Depression"];
 var infoParagraphInput = "My Name is Matt and I was diagnosed with Leukemia was I 15, but I have been in remission for the last 2 and 1/2 years. :-)";
 
-var MattSaltzman = new person(usrName, fullName, emailInput, conditionsList, infoParagraphInput);
+var MattSaltzman = new Mentor(usrName, Gender, Max, Past, fullName, Age, emailInput, conditionsList[0], conditionsList[1], conditionsList[2], psswrd, infoParagraphInput);
 
 function writeUserName() {
-	document.write(MattSaltzman.userName + "\n")
+	document.write(MattSaltzman.username + "\n")
 }
 
 function writeName() {
-	document.write(MattSaltzman.Name + "\n")
+	document.write(MattSaltzman.fullName + "\n")
 }
 
 function writeEmail() {
